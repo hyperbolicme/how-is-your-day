a0673a2e696ce3c28d6efe75988f1216cfda97a3
anju s <anjusrinivasan007@gmail.com>
2025-09-07 20:06:38 +0530

    adding cloudfront domain in origin

--- backend/src/middleware/cors.js
+++ backend/src/middleware/cors.js
@@ -5,7 +5,8 @@ const { config } = require('../config/environment');
 const corsOptions = {
   origin: [
     process.env.FRONTEND_URL  || 'http://localhost:5001',
-    'http://how-is-your-day-frontend-hyperbolicme.s3-website.ap-south-1.amazonaws.com'
+    'http://how-is-your-day-frontend-hyperbolicme.s3-website.ap-south-1.amazonaws.com',
+    'https://d2qr9yt8ob8ckr.cloudfront.net'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
